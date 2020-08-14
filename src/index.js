44575cca7f21460132c9806882666581d2653bec
Arizal Sykes <arizalinc@gmail.com>
2020-08-14 13:12:40 +0700

    update controller and endpoint

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ const route = require('express').Router()
 // import route disini
 const product = require('./routes/product')
 const category = require('./routes/category')
+const order = require('./routes/order')
+const history = require('./routes/history')
 
 // buat middle disini
 route.use('/product', product)
 route.use('/category', category)
+route.use('/order', order)
+route.use('/history', history)
 
 module.exports = route
